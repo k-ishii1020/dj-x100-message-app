18a145744ab9cfc2c3d1c96dbc7654f03758bc7b
k-ishii1020 <8492.business.kazuki7126@gmail.com>
2023-05-11 03:58:12 +0900

    初回リリース

--- X100-Message/Form1.Designer.cs
+++ X100-Message/Form1.Designer.cs
@@ -28,12 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            menuStrip1 = new MenuStrip();
+            ファイルToolStripMenuItem = new ToolStripMenuItem();
+            終了ToolStripMenuItem = new ToolStripMenuItem();
+            logTextBox = new TextBox();
+            statusStrip1 = new StatusStrip();
+            warnLabel = new ToolStripStatusLabel();
+            toolStripStatusLabel2 = new ToolStripStatusLabel();
+            connectBtn = new Button();
+            comComboBox = new ComboBox();
+            comLabel = new Label();
+            msgOutputBtn = new Button();
+            logFileOutFlg = new CheckBox();
+            menuStrip1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { ファイルToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1126, 24);
+            menuStrip1.TabIndex = 4;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            ファイルToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { 終了ToolStripMenuItem });
+            ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            ファイルToolStripMenuItem.Size = new Size(67, 20);
+            ファイルToolStripMenuItem.Text = "ファイル(&F)";
+            // 
+            // 終了ToolStripMenuItem
+            // 
+            終了ToolStripMenuItem.Name = "終了ToolStripMenuItem";
+            終了ToolStripMenuItem.Size = new Size(113, 22);
+            終了ToolStripMenuItem.Text = "終了(&X)";
+            終了ToolStripMenuItem.Click += 終了ToolStripMenuItem_Click;
+            // 
+            // logTextBox
+            // 
+            logTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            logTextBox.BackColor = SystemColors.Info;
+            logTextBox.Location = new Point(12, 27);
+            logTextBox.MinimumSize = new Size(400, 200);
+            logTextBox.Multiline = true;
+            logTextBox.Name = "logTextBox";
+            logTextBox.ReadOnly = true;
+            logTextBox.ScrollBars = ScrollBars.Vertical;
+            logTextBox.Size = new Size(1102, 631);
+            logTextBox.TabIndex = 5;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { warnLabel, toolStripStatusLabel2 });
+            statusStrip1.Location = new Point(0, 706);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(1126, 22);
+            statusStrip1.TabIndex = 6;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // warnLabel
+            // 
+            warnLabel.Name = "warnLabel";
+            warnLabel.Size = new Size(87, 17);
+            warnLabel.Text = "接続していません";
+            // 
+            // toolStripStatusLabel2
+            // 
+            toolStripStatusLabel2.ImageAlign = ContentAlignment.BottomCenter;
+            toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            toolStripStatusLabel2.Size = new Size(993, 17);
+            toolStripStatusLabel2.Spring = true;
+            toolStripStatusLabel2.Text = " （秘密の保護）第59条 何人も法律に別段の定めがある場合を除くほか、特定の相手方に対して行われる無線通信を傍受してその存在若しくは内容を漏らし、又はこれを窃用してはならない。";
+            toolStripStatusLabel2.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // connectBtn
+            // 
+            connectBtn.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            connectBtn.Location = new Point(12, 664);
+            connectBtn.Name = "connectBtn";
+            connectBtn.Size = new Size(75, 23);
+            connectBtn.TabIndex = 7;
+            connectBtn.Text = "接続";
+            connectBtn.UseVisualStyleBackColor = true;
+            connectBtn.Click += ConnectBtn_Click;
+            // 
+            // comComboBox
+            // 
+            comComboBox.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            comComboBox.FormattingEnabled = true;
+            comComboBox.Location = new Point(291, 664);
+            comComboBox.Name = "comComboBox";
+            comComboBox.Size = new Size(121, 23);
+            comComboBox.TabIndex = 8;
+            // 
+            // comLabel
+            // 
+            comLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            comLabel.AutoSize = true;
+            comLabel.Location = new Point(225, 668);
+            comLabel.Name = "comLabel";
+            comLabel.Size = new Size(60, 15);
+            comLabel.TabIndex = 9;
+            comLabel.Text = "COMポート";
+            // 
+            // msgOutputBtn
+            // 
+            msgOutputBtn.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            msgOutputBtn.Enabled = false;
+            msgOutputBtn.Location = new Point(102, 664);
+            msgOutputBtn.Name = "msgOutputBtn";
+            msgOutputBtn.Size = new Size(117, 23);
+            msgOutputBtn.TabIndex = 10;
+            msgOutputBtn.Text = "メッセージ出力開始";
+            msgOutputBtn.UseVisualStyleBackColor = true;
+            msgOutputBtn.Click += MsgOutputBtn_Click;
+            // 
+            // logFileOutFlg
+            // 
+            logFileOutFlg.AutoSize = true;
+            logFileOutFlg.Location = new Point(418, 666);
+            logFileOutFlg.Name = "logFileOutFlg";
+            logFileOutFlg.Size = new Size(68, 19);
+            logFileOutFlg.TabIndex = 11;
+            logFileOutFlg.Text = "ログ出力";
+            logFileOutFlg.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1126, 728);
+            Controls.Add(logFileOutFlg);
+            Controls.Add(msgOutputBtn);
+            Controls.Add(comLabel);
+            Controls.Add(comComboBox);
+            Controls.Add(connectBtn);
+            Controls.Add(statusStrip1);
+            Controls.Add(logTextBox);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            MinimumSize = new Size(600, 400);
+            Name = "Form1";
+            Text = "DJ-X100 メッセージロガー";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem ファイルToolStripMenuItem;
+        private ToolStripMenuItem 終了ToolStripMenuItem;
+        private TextBox logTextBox;
+        private StatusStrip statusStrip1;
+        private Button connectBtn;
+        private ComboBox comComboBox;
+        private Label comLabel;
+        private Button msgOutputBtn;
+        private CheckBox logFileOutFlg;
+        private ToolStripStatusLabel warnLabel;
+        private ToolStripStatusLabel toolStripStatusLabel2;
     }
 }
--- X100-Message/Form1.cs
+++ X100-Message/Form1.cs
@@ -1,10 +1,201 @@
+using System.IO.Ports;
+using System.Text;
+
+
+
 namespace X100_Message
 {
     public partial class Form1 : Form
     {
+
+        private static string DJ_X100_OPEN = "AL~DJ-X100\r";
+        private SerialPort serialPort;
+
+        private bool firstConnectFlg = true;
+        private bool djx100ConnextFlg = false;
+
         public Form1()
         {
             InitializeComponent();
+
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitComPort();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+            Application.Exit();
+        }
+
+        // 無線機に接続
+        private async void ConnectBtn_Click(object sender, EventArgs e)
+        {
+
+            if (connectBtn.Text.Equals("切断"))
+            {
+                CloseConnection();
+                return;
+            }
+            if (InitSerialPort())
+            {
+                await Task.Delay(500);
+
+                if (djx100ConnextFlg)
+                {
+                    connectBtn.Text = "切断";
+                    msgOutputBtn.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("接続エラーが発生しました: ", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    CloseConnection();
+                }
+
+            }
+        }
+
+        private bool InitSerialPort()
+        {
+            // COMポートの設定
+            var portName = comComboBox.Text;
+            var baudRate = 9600;
+            var dataBits = 8;
+            var parity = Parity.None;
+            var stopBits = StopBits.One;
+
+            serialPort = new SerialPort(portName, baudRate, parity, dataBits, stopBits);
+            serialPort.DtrEnable = true;
+            serialPort.RtsEnable = true;
+
+            serialPort.DataReceived += SerialPort_DataReceived;
+
+            try
+            {
+                serialPort.Open();
+                var open = Encoding.ASCII.GetBytes(DJ_X100_OPEN);
+                serialPort.BaseStream.WriteAsync(open, 0, open.Length);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("接続エラーが発生しました: " + ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                serialPort.Close();
+                return false;
+            }
+            return true;
+        }
+
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var serialPort = (SerialPort)sender;
+            var buffer = new byte[serialPort.BytesToRead];
+            var bytesRead = serialPort.Read(buffer, 0, buffer.Length);
+            var response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+            if (firstConnectFlg)
+            {
+                if (response.Equals("\r\nOK\r\n"))
+                {
+                    djx100ConnextFlg = true;
+                    firstConnectFlg = false;
+                    warnLabel.Text = "DJ-X100接続済み";
+                }
+                else
+                {
+                    warnLabel.Text = "接続失敗";
+                }
+                return;
+            }
+            if (response.Equals("\r\nOK\r\n"))
+            {
+                return;
+            }
+
+            // 受信データをテキストボックスに表示（UIスレッドで実行）
+            this.Invoke(new Action(() =>
+
+            {
+
+                DateTime now = DateTime.Now;
+                logTextBox.AppendText($"{now} >> {response}\r\n");
+
+                if (logFileOutFlg.Checked)
+                {
+                    try
+                    {
+                        File.AppendAllText($"received_message_{now.ToString("yyyyMMdd")}.txt", $"{now} >> {response}" + Environment.NewLine);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("メッセージログ出力エラー " + ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+            }));
+        }
+
+        private void CloseConnection()
+        {
+            serialPort.Close();
+            MessageBox.Show("再接続する場合はDJ-X100を再起動してください", "", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            warnLabel.Text = "接続していません";
+            connectBtn.Text = "接続";
+            msgOutputBtn.Text = "メッセージ出力開始";
+            msgOutputBtn.Enabled = false;
+            firstConnectFlg = true;
+
+        }
+
+        private async void MsgOutputBtn_Click(object sender, EventArgs e)
+        {
+            if (msgOutputBtn.Text.Equals("メッセージ出力終了") && serialPort.IsOpen)
+            {
+                CloseConnection();
+                return;
+            }
+
+            msgOutputBtn.Text = "メッセージ出力終了";
+
+            var open = Encoding.ASCII.GetBytes(DJ_X100_OPEN);
+            await serialPort.BaseStream.WriteAsync(open, 0, open.Length);
+            await Task.Delay(100);
+
+            var thru = Encoding.ASCII.GetBytes("AL~THRU\r");
+            await serialPort.BaseStream.WriteAsync(thru, 0, thru.Length);
+        }
+
+
+
+        // COMポート一覧の初期化処理
+        private void InitComPort()
+        {
+            String[] portList = SerialPort.GetPortNames();
+
+            foreach (String portName in portList)
+            {
+                comComboBox.Items.Add(portName);
+            }
+            if (comComboBox.Items.Count > 0)
+            {
+                comComboBox.SelectedIndex = 0;
+            }
+        }
+
+        private void 終了ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+            Application.Exit();
+        }
+
     }
+
+
 }
